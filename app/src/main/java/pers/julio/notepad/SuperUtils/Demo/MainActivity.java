ffb115d6a79e6c04e488e3f5abbd2227a291c46f
chenjiufu <309953422@qq.com>
2020-05-07 17:21:57 +0800

    添加依赖

--- app/src/main/java/pers/julio/notepad/SuperUtils/Demo/MainActivity.java
+++ app/src/main/java/pers/julio/notepad/SuperUtils/Demo/MainActivity.java
@@ -1,6 +1,8 @@
 package pers.julio.notepad.SuperUtils.Demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import pers.julio.notepad.SuperUtils.SpUtils;
+import pers.julio.notepad.SuperUtils.ToastUtils;
 
 import android.os.Bundle;
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastUtils.showToast(this, "测试");
+        SpUtils.getInt(this, "3", 5);
     }
 }
